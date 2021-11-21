8cd62b2932d49b2d40801d18e6783fc455442b63
Furqan Noor <82640979+FurqanNoor@users.noreply.github.com>
2021-11-21 12:54:55 +0200

    Add files via upload

---
+++ snake/game.js
@@ -0,0 +1,51 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+
+/* this is the function for the game to end if the player loses */
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/'
+    }
+    return
+  }
+
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
+}
+
+window.requestAnimationFrame(main)
+
+/* all games are mostly based on 2 functions: a function that updates everything
+  and a function that draws everything. on the update function here it's updating 
+  everything about the snake, food and the function to check for death */
+function update() {
+  updateSnake()
+  updateFood()
+  checkDeath()
+}
+
+function draw() {
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+/* this is the function where if the snake head touches the outside grid,
+  the game ends. */
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
